51ca54ca7908ad28ad9a7141ed3c12daa85c66ae
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-03-08 02:51:25 +0530

    Create visibility modifiers

---
+++ parser/Visibility.h
@@ -0,0 +1,13 @@
+//
+// Created by justinw on 08/03/2026.
+//
+
+#ifndef PYC___VISIBILITY_H
+#define PYC___VISIBILITY_H
+
+
+enum class Visibility {
+    PUBLIC, PRIVATE, INTERNAL
+};
+
+#endif //PYC___VISIBILITY_H
