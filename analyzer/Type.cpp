19493b3cab650c6ada8636ac0f07af34f133821d
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-03-13 01:28:33 +0530

    Implement a basic type system

---
+++ analyzer/Type.cpp
@@ -0,0 +1,55 @@
+//
+// Created by justinw on 12/03/2026.
+//
+
+#include "Type.h"
+
+Type Type::Int() {
+    return {TypeKind::INT};
+}
+
+Type Type::Float() {
+    return {TypeKind::FLOAT};
+}
+
+Type Type::String() {
+    return {TypeKind::STRING};
+}
+
+Type Type::Bool() {
+    return {TypeKind::BOOL};
+}
+
+Type Type::None() {
+    return {TypeKind::NONE};
+}
+
+Type Type::Class(std::string className, std::vector<std::shared_ptr<Type>> wrappedTypes) {
+    return {TypeKind::CLASS, std::move(className), std::move(wrappedTypes)};
+}
+
+Type Type::Function(std::shared_ptr<Type> returnType, std::vector<std::shared_ptr<Type>> paramTypes) {
+    std::vector vec = {std::move(returnType)};
+    vec.insert(vec.end(), paramTypes.begin(), paramTypes.end());
+    return {TypeKind::FUNCTION, "", std::move(vec)};
+}
+
+Type Type::Any() {
+    return {TypeKind::ANY};
+}
+
+Type Type::Error() {
+    return {TypeKind::ERROR};
+}
+
+bool Type::operator==(const Type &other) const {
+    if (kind != other.kind) return false;
+    if (kind==TypeKind::CLASS || kind==TypeKind::FUNCTION) {
+        if (kind==TypeKind::CLASS && className != other.className) return false;
+        if (wrappedTypes.size() != other.wrappedTypes.size()) return false;
+        for (int i=0;i<wrappedTypes.size();++i) {
+            if (*wrappedTypes[i] != *other.wrappedTypes[i]) return false;
+        }
+    }
+    return true;
+}
---
+++ analyzer/Type.h
@@ -0,0 +1,33 @@
+//
+// Created by justinw on 12/03/2026.
+//
+
+#ifndef TYPHON_TYPE_H
+#define TYPHON_TYPE_H
+#include <memory>
+#include <string>
+#include <vector>
+
+enum class TypeKind {
+    INT, FLOAT, STRING, BOOL, NONE, CLASS, FUNCTION, ERROR, ANY
+};
+
+struct Type {
+    TypeKind kind;
+    std::string className;
+    std::vector<std::shared_ptr<Type>> wrappedTypes;
+
+    static Type Int();
+    static Type Float();
+    static Type String();
+    static Type Bool();
+    static Type None();
+    static Type Class(std::string className, std::vector<std::shared_ptr<Type>> wrappedTypes);
+    static Type Function(std::shared_ptr<Type> returnType, std::vector<std::shared_ptr<Type>> paramTypes);
+    static Type Any();
+    static Type Error();
+
+    bool operator==(const Type &other) const;
+};
+
+#endif //TYPHON_TYPE_H
