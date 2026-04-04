df1e1db6f3aa8bbf7aef081734b1a3bd61343134
Justin <84982572+JustINCodingUK@users.noreply.github.com>
2026-04-05 01:29:53 +0530

    Initialize GTest

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,9 @@
 //
 // Created by justinw on 13/03/2026.
 //
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
